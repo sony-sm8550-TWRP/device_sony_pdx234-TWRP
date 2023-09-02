20ed4df9f986b8a7dfd476337acc766c07f51b66
nebrassy <nebras30@gmail.com>
2023-09-02 10:10:50 +0200

    mondrian: remove prebuilt kernel modules
      in this current case modules should be loaded from vendor_dlkm.

--- device-mondrian.mk
+++ device-mondrian.mk
@@ -24,7 +24,3 @@
 $(call inherit-product, $(COMMON_PATH)/device-common.mk)
 
 TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)/twrp
-# Copy modules for depmod
-
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/prebuilt,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
